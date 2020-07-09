4258abab37e41c06cbf1377dd0ba56b10c0ea70e
vil-shavetail <demid.ivan@gmail.com>
2020-07-09 09:41:08 +0300

    Initial commit

---
+++ src/MainClass.java
@@ -0,0 +1,18 @@
+public class MainClass {
+
+    private final int class_number = 20;
+    private final String class_string = "Hello world";
+
+    public int getLocalNumber() {
+        return 14;
+    }
+
+    public int getClassNumber() {
+        return this.class_number;
+    }
+
+    public String getClassString() {
+        return this.class_string;
+    }
+
+}
---
+++ src/MainClassTest.java
@@ -0,0 +1,23 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MainClassTest extends MainClass {
+
+    @Test
+    public void testGetLocalNumber() {
+        Assert.assertEquals("The return value is not equal to 14",
+                14, getLocalNumber());
+    }
+
+    @Test
+    public void testGetClassNumber() {
+        Assert.assertTrue("The return value is less than 45",
+                this.getClassNumber() > 45);
+    }
+
+    @Test
+    public void testGetClassString() {
+        Assert.assertTrue("The returned string doesn,t contain a \"Hello\" or \"hello\" substring.",
+                this.getClassString().contains("Hello") || this.getClassString().contains("hello"));
+    }
+}
