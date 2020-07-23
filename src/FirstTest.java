d6e564468544b526ba716e6a7c028f0b5bb3de81
vil-shavetail <demid.ivan@gmail.com>
2020-07-23 16:49:02 +0300

    Update FirstTest, Clearing the input field
    Second block. Simple scripts in Appium.
    FirstTest, Clearing the input field.

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -64,6 +64,19 @@ public class FirstTest {
                 5
         );
 
+        waitForElementAndSendKeys(
+                By.xpath("//*[contains(@text, 'Search…')]"),
+                "Java",
+                "Cannot find search input",
+                5
+        );
+
+        waitForElementAndClear(
+                By.id("org.wikipedia:id/search_src_text"),
+                "Cannot find search field",
+                5
+        );
+
         waitForElementAndClick(
                 By.id("org.wikipedia:id/search_close_btn"),
                 "Cannot find X to cancel search",
@@ -103,6 +116,11 @@ public class FirstTest {
         WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
         wait.withMessage(error_message + "\n");
         return wait.until(ExpectedConditions.invisibilityOfElementLocated(by));
+    }
+    private WebElement waitForElementAndClear(By by, String error_messag, long timeoutInSeconds) {
+        WebElement element = waitForElementPresent(by, error_messag, timeoutInSeconds);
+        element.clear();
+        return element;
     }
 
 }
