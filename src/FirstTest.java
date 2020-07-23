502dcf9117f7e609d8d91066bb0dcc9b34ddc0e2
vil-shavetail <demid.ivan@gmail.com>
2020-07-23 13:18:20 +0300

    FirstTest add sendKeys
    Second block. Simple scripts in Appium.
    First Test add sendKeys

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -33,9 +33,11 @@ public class FirstTest {
 
     @Test
     public void firstTest() {
-        WebElement element = driver.findElementByXPath("//*[contains(@text, 'Search Wikipedia')]");
-        element.click();
-//        System.out.println("First test run");
+        WebElement element_to_init_search = driver.findElementByXPath("//*[contains(@text, 'Search Wikipedia')]");
+        element_to_init_search.click();
+
+        WebElement element_to_enter_search_line = driver.findElementByXPath("//*[contains(@text, 'Search…')]");
+        element_to_enter_search_line.sendKeys("Appium");
     }
 
 }
