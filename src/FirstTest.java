d5fd707fc7aa6f59185a16b0c42cf085e0788281
vil-shavetail <demid.ivan@gmail.com>
2020-07-29 23:42:57 +0300

    Ex7*
    Added driver.rotate(ScreenOrientation.PORTRAIT) in the @Before section;

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -32,6 +32,7 @@ public class FirstTest {
         capabilities.setCapability("app", "/Users/ethandemidovich/Desktop/JavaAppiumAutomation/apks/org.wikipedia.apk");
 
         driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        driver.rotate(ScreenOrientation.PORTRAIT);
     }
 
     @After
