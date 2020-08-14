7882db44cd33ae5cd06ab694efe9e0cc66a24ba7
vil-shavetail <demid.ivan@gmail.com>
2020-08-14 23:44:41 +0300

    Change Platform
    Fifth block - iOS Settings
    05. Change Platform. Added methods are: getPlatformDriver, getCapabilitiesByPlatformEnv.

--- src/lib/CoreTestCase.java
+++ src/lib/CoreTestCase.java
@@ -2,14 +2,19 @@ package lib;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.ios.IOSDriver;
 import junit.framework.TestCase;
 import org.openqa.selenium.ScreenOrientation;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.Duration;
 
 public class CoreTestCase extends TestCase {
+
+    private static final String PLATFORM_IOS = "ios";
+    private static final String PLATFORM_ANDROID = "android";
 
     protected AppiumDriver driver;
     private static String AppiumUrl = "http://127.0.0.1:4723/wd/hub";
@@ -18,17 +23,9 @@ public class CoreTestCase extends TestCase {
     protected void setUp() throws Exception {
         super.setUp();
 
-        DesiredCapabilities capabilities = new DesiredCapabilities();
+        DesiredCapabilities capabilities = this.getCapabilitiesByPlatformEnv();
 
-        capabilities.setCapability("platformName", "Android");
-        capabilities.setCapability("deviceName", "AndroidTestDevice");
-        capabilities.setCapability("platformVersion", "8.1");
-        capabilities.setCapability("automationName", "Appium");
-        capabilities.setCapability("appPackage", "org.wikipedia");
-        capabilities.setCapability("appActivity", ".main.MainActivity");
-        capabilities.setCapability("app", "/Users/ethandemidovich/Desktop/JavaAppiumAutomation/apks/org.wikipedia.apk");
-
-        driver = new AndroidDriver(new URL(AppiumUrl), capabilities);
+        driver = this.getPlatformDriver(capabilities);
         this.rotateScreenPortrait();
     }
 
@@ -49,6 +46,42 @@ public class CoreTestCase extends TestCase {
 
     protected void backgroundApp(int seconds) {
         driver.runAppInBackground(Duration.ofDays(seconds));
+    }
+
+    private DesiredCapabilities getCapabilitiesByPlatformEnv() throws Exception {
+        String platform = System.getenv("PLATFORM");
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        if(platform.equals(PLATFORM_ANDROID)) {
+            capabilities.setCapability("platformName", "Android");
+            capabilities.setCapability("deviceName", "AndroidTestDevice");
+            capabilities.setCapability("platformVersion", "8.1");
+            capabilities.setCapability("automationName", "Appium");
+            capabilities.setCapability("appPackage", "org.wikipedia");
+            capabilities.setCapability("appActivity", ".main.MainActivity");
+            capabilities.setCapability("app", "/Users/ethandemidovich/Desktop/JavaAppiumAutomation/apks/org.wikipedia.apk");
+        } else if(platform.equals(PLATFORM_IOS)) {
+            capabilities.setCapability("platformName", "iOS");
+            capabilities.setCapability("deviceName", "iPhone SE (1st generation)");
+            capabilities.setCapability("platformVersion", "13.6");
+            capabilities.setCapability("app", "/Users/ethandemidovich/Desktop/JavaAppiumAutomation/apks/Wikipedia.app");
+        } else {
+            throw new Exception("Cannot get run platform from env variable. Platform value " + platform);
+        }
+        return  capabilities;
+    }
+
+    private AppiumDriver getPlatformDriver(DesiredCapabilities capabilities) throws Exception {
+        String platform = System.getenv("PLATFORM");
+
+        if(platform.equals(PLATFORM_ANDROID)) {
+            driver = new AndroidDriver(new URL(AppiumUrl), capabilities);
+        } else if(platform.equals(PLATFORM_IOS)) {
+            driver = new IOSDriver(new URL(AppiumUrl), capabilities);
+        } else {
+            throw new Exception("Cannot get platform driver for platform value " + platform);
+        }
+        return  driver;
     }
 
 }
