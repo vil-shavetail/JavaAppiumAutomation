e6d275e6d6fcef490b9dd44ebe1ce1b9334c1478
vil-shavetail <demid.ivan@gmail.com>
2020-08-19 21:56:25 +0300

    Search Tests Refactoring
    Sixth block - Work with iOS
    02. Search Tests Refactoring. Added AndroidSearchPageObject and IOSSearchPageObject.

--- src/lib/ui/SearchPageObject.java
+++ src/lib/ui/SearchPageObject.java
@@ -2,16 +2,16 @@ package lib.ui;
 
 import io.appium.java_client.AppiumDriver;
 
-public class SearchPageObject extends MainPageObject {
+abstract public class SearchPageObject extends MainPageObject {
 
-    private final static String
-            SEARCH_INIT_ELEMENT = "xpath://*[contains(@text, 'Search Wikipedia')]",
-            SEARCH_INPUT = "xpath://*[contains(@text, 'Search…')]",
-            SEARCH_CANCEL_BUTTON = "id:org.wikipedia:id/search_close_btn",
-            SEARCH_RESULT_BY_SUBSTRING_TPL = "xpath://*[@resource-id='org.wikipedia:id/page_list_item_container']//*[@text='{SUBSTRING}']",
-            SEARCH_RESULT_BY_TWO_SUBSTRING_TPL = "xpath://*[@resource-id='org.wikipedia:id/page_list_item_container']//*[@text='{TITLE}']//..//*[@text='{DESCRIPTION}']",
-            SEARCH_RESULT_ELEMENT = "xpath://*[@resource-id='org.wikipedia:id/search_results_list']/*[@resource-id='org.wikipedia:id/page_list_item_container']",
-            SEARCH_EMPTY_RESULT_ELEMENT = "xpath://*[@text='No results found']";
+    protected static String
+            SEARCH_INIT_ELEMENT,
+            SEARCH_INPUT,
+            SEARCH_CANCEL_BUTTON,
+            SEARCH_RESULT_BY_SUBSTRING_TPL,
+            SEARCH_RESULT_BY_TWO_SUBSTRING_TPL,
+            SEARCH_RESULT_ELEMENT,
+            SEARCH_EMPTY_RESULT_ELEMENT;
 
     public SearchPageObject(AppiumDriver driver) {
         super(driver);
---
+++ src/lib/ui/ios/IOSSearchPageObject.java
@@ -0,0 +1,21 @@
+package lib.ui.ios;
+
+import io.appium.java_client.AppiumDriver;
+import lib.ui.SearchPageObject;
+
+public class IOSSearchPageObject extends SearchPageObject {
+    static {
+        SEARCH_INIT_ELEMENT = "xpath://XCUIElementTypeSearchField[@name='Search Wikipedia']";
+        SEARCH_INPUT = "xpath://XCUIElementTypeSearchField";
+        SEARCH_CANCEL_BUTTON = "id:Close";
+        SEARCH_RESULT_BY_SUBSTRING_TPL = "xpath://XCUIElementTypeStaticText[@name='{SUBSTRING}']";
+        SEARCH_RESULT_BY_TWO_SUBSTRING_TPL = "xpath://XCUIElementTypeStaticText[@name='{SUBSTRING}']//..//XCUIElementTypeStaticText[@name='{DESCRIPTION}']";
+        SEARCH_RESULT_ELEMENT = "xpath://XCUIElementTypeStaticText";
+        SEARCH_EMPTY_RESULT_ELEMENT = "id:No results found";
+    }
+
+    public IOSSearchPageObject(AppiumDriver driver) {
+        super(driver);
+    }
+
+}
